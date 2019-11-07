a2502eb8e683aeaa0804a1d6ae6ff27b5305f4a5
kiva12138 <sunhaoxx@foxmail.com>
2019-11-07 12:03:34 +0800

    导航栏基本定型

---
+++ erpfe/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class='app_class'>
+
+    <div class='navbar_class'>
+      <navbar/>
+    </div>
+
+    <router-view class='router_view_class'/>
+
+  </div>
+</template>
+
+<script>
+
+import navbar from './components/nav_components/navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'navbar': navbar
+  }
+}
+
+</script>
+
+<style>
+body{
+  margin: 0px;
+}
+.navbar_class {
+  width: 15%;
+  height: 100%;
+  position: fixed;
+  display: inline-block;
+  background-color: #545c64;
+  overflow-x: hidden;
+  overflow-y: auto;
+  vertical-align: top;
+}
+.router_view_class {
+  margin-left: 15%;
+  width: 84%;
+  position: fixed;
+  background-color: green;
+  display: inline-block;
+  vertical-align: top;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+  background-color: #566573;
+}
+
+::-webkit-scrollbar-track {
+  border-radius:10px;
+  background-color:#566573;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius:10px;
+  background-color: #273746;
+
+}
+</style>
---
+++ erpfe/src/components/nav_components/navbar.vue
@@ -0,0 +1,104 @@
+<template>
+<div>
+  <div class="logo_div"> Logo And Login </div>
+  <el-menu class="nav_el_menu"
+    background-color="#545c64"
+text-color="#fff"
+active-text-color="#ffd04b"
+    @select="handleSelect">
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="1-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航二</span>
+      </template>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航三</span>
+      </template>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="3-1">选项1</el-menu-item>
+        <el-menu-item index="3-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="3-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="3-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="3-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-submenu index="4">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航四</span>
+      </template>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="4-1">选项1</el-menu-item>
+        <el-menu-item index="4-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="4-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="4-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="4-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+  </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style>
+.logo_div {
+background-color: green;
+margin: 20px;
+}
+.nav_el_menu {
+margin-right: -1px;
+}
+</style>
